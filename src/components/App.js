8a79d9a4ed769915542173dc9e47234bba988e5a
shubhamkumar1920 <kshubham291@gmail.com>
2021-02-26 00:16:38 +0530

    adding

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ const App = () => {
   });
   const [submitted,setSubmitted] = useState(false);
   const [valid,setValid] = useState(false);
+  const [error,setError] = useState(false);
 
   const handleNameInputChange = (event) => {
     setValues({...values,name: event.target.value});
@@ -41,40 +42,51 @@ const App = () => {
     if(values.name && values.email  && values.phonenumber && values.password && values.password.length > 5 && values.email.includes("@") && isNaN(values.name))
     {
       setValid(true);
+      setError(false);
+    }
+    if(!isNaN(values.name)){
+      setError(true);
+    }else if(!values.name){
+      setError(true);
+    }else if(values.email && !values.email.includes("@")){
+      setError(true);
+    }else if(!values.email){
+      setError(true);
+    }else if(!values.phonenumber){
+      setError(true);
+    }else if(values.password && document.getElementById('password').value.length < 6){
+      setError(true);
+    }else if(!values.password){
+      setError(true);
+    }
+    else{
+      setError(false);
     }
     setSubmitted(true);
   }
   return (
     <div id="main">
       <form id="register-form" onSubmit={handleSubmit}>
-        {submitted && valid ? <div className="success-message">Hello {values.email.split('@')[0]}</div> : null}
+        {submitted && valid && !error ? <div className="success-message">Hello {values.email.split('@')[0]}</div> : null}
         <input data-testid='name' className="form-field" onChange={handleNameInputChange} 
-        value={values.name}
-        type="text"
-        placeholder="Name"
-        name="Name"
-        required>
+        value={values.name} type="text" placeholder="Name" name="Name">
         </input>
-        {submitted && !values.name ? <span>Name Error</span> : null}
-        {submitted && values.name && !isNaN(values.name) ? <span>Name is not alphanumeric</span> : null}
+        {error && !values.name ? <span>Name Error</span> : null}
+        {error && values.name && !isNaN(values.name) ? <span>Name is not alphanumeric</span> : null}
         <input data-testid='email' className="form-field" value={values.email} onChange={handleEmailInputChange}
-        type="email"
-        placeholder="Email"
-        name="email"
-        required>
-        </input>
-        {submitted && !values.email ? <span>Email Error</span> : null}
-        {submitted && values.email && !values.email.includes("@") ? <span>Email must contain @</span> : null}
+        type="email" placeholder="Email" name="email"></input>
+        {error && !values.email ? <span>Email Error</span> : null}
+        {error && values.email && !values.email.includes("@") ? <span>Email must contain @</span> : null}
         <select data-testid='gender' className="form-field" value={values.gender} onChange={handleGenderInputChange}>
             <option defaultValue="male">male</option>
             <option value="female">female</option>
             <option value="others">others</option>
         </select>
-        <input data-testid='phoneNumber' required className="form-field" value={values.phonenumber} onChange={handlePhoneNumberInputChange} type="number" placeholder="phone" ></input>
-        {submitted && !values.phonenumber ? <span>Phone Number Error</span> : null}
-        <input data-testid='password' id="password" type='password' className="form-field" value={values.password} onChange={handlePasswordInputChange} placeholder="password" required></input>
-        {submitted && !values.password ? <span>Password Error</span> : null}
-        {submitted && values.password && document.getElementById('password').value.length < 6 ? <span>Password must contain atleast 6 letters</span> : null}
+        <input data-testid='phoneNumber' className="form-field" value={values.phonenumber} onChange={handlePhoneNumberInputChange} type="number" placeholder="phone" ></input>
+        {error && !values.phonenumber ? <span>Phone Number Error</span> : null}
+        <input data-testid='password' id="password" type='password' className="form-field" value={values.password} onChange={handlePasswordInputChange} placeholder="password"></input>
+        {error && !values.password ? <span>Password Error</span> : null}
+        {error && values.password && document.getElementById('password').value.length < 6 ? <span>Password must contain atleast 6 letters</span> : null}
         <button data-testid='submit' className="form-field" id="submit" placeholder="Submit">Submit</button>
       </form>
     </div>
