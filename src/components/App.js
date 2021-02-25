be6e5b71cdc37541232004e2aa3ba544d189057f
shubhamkumar1920 <kshubham291@gmail.com>
2021-02-26 01:28:34 +0530

    adding:

--- src/components/App.js
+++ src/components/App.js
@@ -13,37 +13,24 @@ const App = () => {
   const [valid,setValid] = useState(false);
   const [error,setError] = useState(false);
 
-  const handleNameInputChange = (event) => {
-    setValues({...values,name: event.target.value});
-    setValid(false);
-  };
 
-  const handleEmailInputChange = (event) => {
-    setValues({...values,email: event.target.value});
-    setValid(false);
-  };
-
-  const handleGenderInputChange = (event) => {
-    setValues({...values,gender: event.target.value});
-    setValid(false);
-  };
-
-  const handlePhoneNumberInputChange = (event) => {
-    setValues({...values,phonenumber: event.target.value});
-    setValid(false);
-  };
-
-  const handlePasswordInputChange = (event) => {
-    setValues({...values,password: event.target.value});
-    setValid(false);
+  const handleOnChange = (event) => {
+    // setError("");
+    // setWelcome("");
+    if (event.target.getAttribute("data-testid") === "name") {
+      setValues({...values,name: event.target.value});
+    } else if (event.target.getAttribute("data-testid") === "email") {
+      setValues({...values,email: event.target.value});
+    } else if (event.target.getAttribute("data-testid") === "gender") {
+      setValues({...values,gender: event.target.value});
+    } else if (event.target.getAttribute("data-testid") === "phoneNumber") {
+      setValues({...values,phonenumber: event.target.value});
+    } else if (event.target.getAttribute("data-testid") === "password") {
+      setValues({...values,password: event.target.value});
+    }
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    if(values.name && values.email  && values.phonenumber && values.password && values.password.length > 5 && values.email.includes("@") && isNaN(values.name))
-    {
-      setValid(true);
-      setError(false);
-    }
     if(!isNaN(values.name)){
       setError(true);
     }else if(!values.name){
@@ -63,28 +50,33 @@ const App = () => {
       setError(false);
     }
     setSubmitted(true);
+    if(values.name && values.email  && values.phonenumber && values.password && values.password.length > 5 && values.email.includes("@") && isNaN(values.name))
+    {
+      setValid(true);
+      setError(false);
+    }
   }
   return (
     <div id="main">
       <form id="register-form" onSubmit={handleSubmit}>
         {submitted && valid && !error ? <div className="success-message">Hello {values.email.split('@')[0]}</div> : null}
-        <input data-testid='name' className="form-field" onChange={handleNameInputChange} 
+        <input data-testid='name' className="form-field" onChange={handleOnChange} 
         value={values.name} type="text" placeholder="Name" name="Name">
         </input>
         {error && !values.name ? <span>Name Error</span> : null}
         {error && values.name && !isNaN(values.name) ? <span>Name is not alphanumeric</span> : null}
-        <input data-testid='email' className="form-field" value={values.email} onChange={handleEmailInputChange}
+        <input data-testid='email' className="form-field" value={values.email} onChange={handleOnChange}
         type="email" placeholder="Email" name="email"></input>
         {error && !values.email ? <span>Email Error</span> : null}
         {error && values.email && !values.email.includes("@") ? <span>Email must contain @</span> : null}
-        <select data-testid='gender' className="form-field" value={values.gender} onChange={handleGenderInputChange}>
+        <select data-testid='gender' className="form-field" value={values.gender} onChange={handleOnChange}>
             <option defaultValue="male">male</option>
             <option value="female">female</option>
             <option value="others">others</option>
         </select>
-        <input data-testid='phoneNumber' className="form-field" value={values.phonenumber} onChange={handlePhoneNumberInputChange} type="number" placeholder="phone" ></input>
+        <input data-testid='phoneNumber' className="form-field" value={values.phonenumber} onChange={handleOnChange} type="number" placeholder="phone" ></input>
         {error && !values.phonenumber ? <span>Phone Number Error</span> : null}
-        <input data-testid='password' id="password" type='password' className="form-field" value={values.password} onChange={handlePasswordInputChange} placeholder="password"></input>
+        <input data-testid='password' id="password" type='password' className="form-field" value={values.password} onChange={handleOnChange} placeholder="password"></input>
         {error && !values.password ? <span>Password Error</span> : null}
         {error && values.password && document.getElementById('password').value.length < 6 ? <span>Password must contain atleast 6 letters</span> : null}
         <button data-testid='submit' className="form-field" id="submit" placeholder="Submit">Submit</button>
