a8aa5167fd05c1150c5c99a0efe6df6416912d43
shubhamkumar1920 <kshubham291@gmail.com>
2021-02-26 17:41:42 +0530

    adding

--- src/components/App.js
+++ src/components/App.js
@@ -11,75 +11,93 @@ const App = () => {
   });
   const [submitted,setSubmitted] = useState(false);
   const [valid,setValid] = useState(false);
-  const [error,setError] = useState(false);
+  const [error,setError] = useState("");
 
 
   const handleOnChange = (event) => {
-    // setError("");
-    // setWelcome("");
+    setError("");
     if (event.target.getAttribute("data-testid") === "name") {
       setValues({...values,name: event.target.value});
+      setValid(false);
     } else if (event.target.getAttribute("data-testid") === "email") {
       setValues({...values,email: event.target.value});
+      setValid(false);
     } else if (event.target.getAttribute("data-testid") === "gender") {
       setValues({...values,gender: event.target.value});
+      setValid(false);
     } else if (event.target.getAttribute("data-testid") === "phoneNumber") {
       setValues({...values,phonenumber: event.target.value});
+      setValid(false);
     } else if (event.target.getAttribute("data-testid") === "password") {
       setValues({...values,password: event.target.value});
+      setValid(false);
     }
+  };
+  const checkError = (name,email,phonenumber,password) => {
+    let list = [];
+    if (name === "" || email === "" || phonenumber === "" || password === "") {
+      list.push("All fields are mandatory");
+    }
+    // name validation
+    let names = name.split("");
+    let temp = names.filter(
+      (x) =>
+        !(
+          (x <= "z" && x >= "a") ||
+          x === " " ||
+          (x <= "Z" && x >= "A") ||
+          (x >= "0" && x <= "9")
+        )
+    );
+    if (temp.length > 0) {
+      list.push("Message: Name is not alphanumeric");
+    }
+    //email validation
+    if (email.indexOf("@") === -1) {
+      list.push("Email must contain @");
+    }
+    //phone Number validation
+    if (!Number(phonenumber)) {
+      list.push("Phone Number must contain only numbers");
+    }
+    // password validation
+    if (password.length < 6) {
+      list.push("Password must contain atleast 6 letters");
+    }
+    return list;
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    if(!isNaN(values.name)){
-      setError(true);
-    }else if(!values.name){
-      setError(true);
-    }else if(values.email && !values.email.includes("@")){
-      setError(true);
-    }else if(!values.email){
-      setError(true);
-    }else if(!values.phonenumber){
-      setError(true);
-    }else if(values.password && document.getElementById('password').value.length < 6){
-      setError(true);
-    }else if(!values.password){
-      setError(true);
-    }
-    else{
-      setError(false);
+    let errors = checkError(values.name, values.email, values.phonenumber, values.password);
+    if (errors.length > 0) {
+      setError(errors[0]);
+      return;
     }
     setSubmitted(true);
     if(values.name && values.email  && values.phonenumber && values.password && values.password.length > 5 && values.email.includes("@") && isNaN(values.name))
     {
       setValid(true);
-      setError(false);
     }
   }
   return (
     <div id="main">
       <form id="register-form" onSubmit={handleSubmit}>
-        {submitted && valid && !error ? <div className="success-message">Hello {values.email.split('@')[0]}</div> : null}
+        {submitted && valid ? <div className="success-message">Hello {values.email.split('@')[0]}</div> : null}
+        {error ? <div className ="error"><h3>{error}</h3></div>: null}
         <input data-testid='name' className="form-field" onChange={handleOnChange} 
         value={values.name} type="text" placeholder="Name" name="Name">
         </input>
-        {error && !values.name ? <span>Name Error</span> : null}
-        {error && values.name && !isNaN(values.name) ? <span>Name is not alphanumeric</span> : null}
         <input data-testid='email' className="form-field" value={values.email} onChange={handleOnChange}
-        type="email" placeholder="Email" name="email"></input>
-        {error && !values.email ? <span>Email Error</span> : null}
-        {error && values.email && !values.email.includes("@") ? <span>Email must contain @</span> : null}
+        type="text" placeholder="Email" name="email"></input>
         <select data-testid='gender' className="form-field" value={values.gender} onChange={handleOnChange}>
             <option defaultValue="male">male</option>
             <option value="female">female</option>
             <option value="others">others</option>
         </select>
-        <input data-testid='phoneNumber' className="form-field" value={values.phonenumber} onChange={handleOnChange} type="number" placeholder="phone" ></input>
-        {error && !values.phonenumber ? <span>Phone Number Error</span> : null}
+        <input data-testid='phoneNumber' className="form-field" value={values.phonenumber} onChange={handleOnChange} type="text" placeholder="phone" ></input>
         <input data-testid='password' id="password" type='password' className="form-field" value={values.password} onChange={handleOnChange} placeholder="password"></input>
-        {error && !values.password ? <span>Password Error</span> : null}
-        {error && values.password && document.getElementById('password').value.length < 6 ? <span>Password must contain atleast 6 letters</span> : null}
         <button data-testid='submit' className="form-field" id="submit" placeholder="Submit">Submit</button>
+       
       </form>
     </div>
   )
